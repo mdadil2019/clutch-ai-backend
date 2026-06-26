1b63811e2b9aac380405dca3ae95327323c844c9
Mohammad Adil <mdadil2019@gmail.com>
2026-06-26 08:51:04 +0300

    updated based on feedback #2

--- src/db/migrations/1781427626287-migration.ts
+++ src/db/migrations/1781427626287-migration.ts
@@ -4,8 +4,17 @@ export class Migration1781427626287 implements MigrationInterface {
     name = 'Migration1781427626287'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "streams" ("id" SERIAL NOT NULL, "streamURL" character varying NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_40440b6f569ebc02bc71c25c499" PRIMARY KEY ("id"))`);
-        await queryRunner.query(`CREATE TABLE "events" ("id" SERIAL NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "streamId" integer NOT NULL, "status" character varying NOT NULL, CONSTRAINT "PK_40731c7151fe4be3116e45ddf73" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "streams" 
+            ("id" SERIAL NOT NULL, 
+            "streamURL" character varying NOT NULL, 
+            "createdAt" TIMESTAMP NOT NULL DEFAULT now(), 
+            CONSTRAINT "PK_streams" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "events" 
+            ("id" SERIAL NOT NULL, 
+            "createdAt" TIMESTAMP NOT NULL DEFAULT now(), 
+            "streamId" integer NOT NULL, 
+            "status" character varying NOT NULL, 
+            CONSTRAINT "PK_events" PRIMARY KEY ("id"))`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
